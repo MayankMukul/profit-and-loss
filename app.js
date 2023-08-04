222876201ba8269a32bb2266c636cdc5f6a576af
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-08-05 00:29:34 +0530

    basic js done

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ var message = document.querySelector(".output");
 
 function calculateProfit(costPrice,sellingPrice,qty){
     var profit = (sellingPrice - costPrice)*qty ;
-    var profitPercent = (profit/costPrice)*100;
+    var profitPercent = ((profit/costPrice)*100).toFixed(2);
     message.innerHTML=`<span style="color:green" > profit = ${profit} profit%= ${profitPercent} `;
 }
 
 function calculateLoss(costPrice,sellingPrice,qty){
     var loss= (costPrice - sellingPrice)*qty;
-    var lossPercent = (loss/costPrice)*100;
+    var lossPercent = ((loss/costPrice)*100).toFixed(2);
     message.innerHTML=`<span style=" color:red" > Loss = ${loss} Loss%= ${lossPercent} `;
 }
 
@@ -24,14 +24,18 @@ function clickHandler(){
     var costPrice = initialPrice.value;
     var sellingPrice = currentPrice.value;
     var qty = stocks.value;
-
-    if(sellingPrice>costPrice){
-        calculateProfit(costPrice,sellingPrice,qty);
-    } else if (sellingPrice<costPrice){
-        calculateLoss(costPrice,sellingPrice,qty);
-    } else if (sellingPrice==costPrice){
-         equal();
+    if (costPrice !== '' && sellingPrice !== '' && qty !== '' ){
+        if(sellingPrice>costPrice){
+            calculateProfit(costPrice,sellingPrice,qty);
+        } else if (sellingPrice<costPrice){
+            calculateLoss(costPrice,sellingPrice,qty);
+        } else if (sellingPrice==costPrice){
+             equal();
+        }
+    } else {
+        alert('Please fill out all fields');
     }
+   
 }
 
 btn.addEventListener("click",clickHandler);
